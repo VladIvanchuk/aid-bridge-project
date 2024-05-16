d91475220970bc5680a9d01159662d2aa32d53ce
Vladislav Ivanchuk <vlad.ivanchuk8888@gmail.com>
2024-05-17 00:44:08 +0300

    fix api

--- app/api/needs/route.ts
+++ app/api/needs/route.ts
@@ -1,14 +1,23 @@
 import dbConnect from "@/lib/mongodb";
 import Need from "@/models/need";
-import { NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function GET() {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   await dbConnect();
-  try {
-    const needs = await Need.find({});
 
-    NextResponse.json(needs);
-  } catch (err: any) {
-    NextResponse.json({ error: err.message });
+  if (req.method === "GET") {
+    try {
+      const needs = await Need.find({});
+      res.status(200).json(needs);
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    // Optionally handle other methods or send a method not allowed response
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
